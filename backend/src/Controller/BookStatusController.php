118db5e6ab3640836a9eb3ec82ebaff7c25823de
sihem Hadj-tahar <60602079+sihemht@users.noreply.github.com>
2026-04-03 14:05:14 +0200

    fix: add missing imports for EntityManager and AsController

--- backend/src/Controller/BookStatusController.php
+++ backend/src/Controller/BookStatusController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Workflow\Registry;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BookStatusController extends AbstractController
 {
