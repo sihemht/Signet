10acc3a5fb61e90e879dbb20158fc212fc734094
sihem Hadj-tahar <60602079+sihemht@users.noreply.github.com>
2026-04-03 13:49:12 +0200

    add env.test and fix working directory for phpstan

--- backend/var/cache/dev/App_KernelDevDebugContainer.php
+++ backend/var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerYEKRRxX\App_KernelDevDebugContainer([
     'container.build_hash' => 'YEKRRxX',
-    'container.build_id' => '45d2c3b6',
-    'container.build_time' => 1774371314,
+    'container.build_id' => '301fa1d5',
+    'container.build_time' => 1775137870,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerYEKRRxX');
--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'bbb0a36e02b381733987fbd6b2580822796de411',
+        'reference' => '4717a5be4e93203ffafd68a6591f0f73f4a0d0fa',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'bbb0a36e02b381733987fbd6b2580822796de411',
+            'reference' => '4717a5be4e93203ffafd68a6591f0f73f4a0d0fa',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -565,8 +565,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache-implementation' => array(
